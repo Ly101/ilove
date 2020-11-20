83fc8ea14889b332d9563d5f17d69b1c99a6d448
alps_glb <1349467950_glb@qq.com>
2020-11-20 16:06:47 +0800

    新建i love项目

---
+++ src/main/java/com/alps/ilove/IloveApplication.java
@@ -0,0 +1,15 @@
+package com.alps.ilove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IloveApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IloveApplication.class, args);
+    }
+
+}
