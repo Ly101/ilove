c22be5f9fd12f75cabd4f61f8abbb6b875e252bb
alps_glb <1349467950_glb@qq.com>
2020-11-21 01:41:59 +0800

    添加子模块ilove_serialservice，用作serial相关的服务

---
+++ ilove_serialservice/src/main/java/com/alps/ilove/IloveSerialserviceApplication.java
@@ -0,0 +1,15 @@
+package com.alps.ilove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IloveSerialserviceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IloveSerialserviceApplication.class, args);
+    }
+
+}
---
+++ ilove_serialservice/src/test/java/com/alps/ilove/IloveSerialserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alps.ilove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IloveSerialserviceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+
+    }
+
+}
