c0a61d257ea9cb942bf347047a9a83a4f00798cf
alps_glb <1349467950_glb@qq.com>
2020-11-24 00:03:43 +0800

    给所有mapper接口添加mapper注解

--- ilove_serialservice/src/main/java/com/alps/ilove/ilove_serialservice/mapper/TblActressSerialMapMapper.java
+++ ilove_serialservice/src/main/java/com/alps/ilove/ilove_serialservice/mapper/TblActressSerialMapMapper.java
@@ -2,6 +2,7 @@ package com.alps.ilove.ilove_serialservice.mapper;
 
 import com.alps.ilove.ilove_serialservice.entity.TblActressSerialMap;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author Ly
  * @since 2020-11-21
  */
+@Mapper
 public interface TblActressSerialMapMapper extends BaseMapper<TblActressSerialMap> {
 
 }
--- ilove_serialservice/src/main/java/com/alps/ilove/ilove_serialservice/mapper/TblStagePhotoMapper.java
+++ ilove_serialservice/src/main/java/com/alps/ilove/ilove_serialservice/mapper/TblStagePhotoMapper.java
@@ -2,6 +2,7 @@ package com.alps.ilove.ilove_serialservice.mapper;
 
 import com.alps.ilove.ilove_serialservice.entity.TblStagePhoto;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author Ly
  * @since 2020-11-21
  */
+@Mapper
 public interface TblStagePhotoMapper extends BaseMapper<TblStagePhoto> {
 
 }
